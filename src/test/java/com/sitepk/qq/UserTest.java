d940089cee613964e9b8be39b0e6f700222aabf1
lineageii <hujiag@gmail.com>
2011-03-30 18:35:50 +0800

    1

--- src/test/java/com/sitepk/qq/UserTest.java
+++ src/test/java/com/sitepk/qq/UserTest.java
@@ -9,13 +9,13 @@ public class UserTest {
 	@Test
 	public void testLogin() throws Exception {
 		User user = new User();
-		user.setId("hujiag@gmail.com");
+		user.setId("hujiag@qq.com");
 		user.setPassword("huyang,./");
 		user.login();
 		
-		//user.sendMsg("1286654658","测试中文消失");
+		user.sendMsg("2608667081","测试中文消息");
 		
-		user.sendGroupMsg("1666263578","我是胡佳。zb");
+		//user.sendGroupMsg("1666263578","我是胡佳。zb");
 		//user.sendGroupMsg("419781577","测试QQ群发消息");
 		//user.sendGroupMsg("829759806","测试QQ群发消息");
 	}
