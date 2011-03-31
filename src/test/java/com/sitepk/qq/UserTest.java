e610f5e26dc94884cc37cb89fdf59c9394401575
lineageii <hujiag@gmail.com>
2011-03-31 18:55:35 +0800

    1

--- src/test/java/com/sitepk/qq/UserTest.java
+++ src/test/java/com/sitepk/qq/UserTest.java
@@ -14,7 +14,7 @@ public class UserTest {
 		user.login();
 		
 		user.sendMsg("2608667081","测试中文消息");
-		
+		//
 		//user.sendGroupMsg("1666263578","我是胡佳。zb");
 		//user.sendGroupMsg("419781577","测试QQ群发消息");
 		//user.sendGroupMsg("829759806","测试QQ群发消息");
