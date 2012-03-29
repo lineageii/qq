ab696f93095b56a1ee654bb4dd46bfab05382752
hujiajia <hujiajia@hujiajia-PC>
2012-03-29 14:30:41 +0800

    1

---
+++ src/main/java/com/sitepk/qq/Group.java
@@ -0,0 +1,29 @@
+package com.sitepk.qq;
+
+public class Group {
+	private String gid;
+	private String name;
+	private String code;
+	
+	
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getGid() {
+		return gid;
+	}
+	public void setGid(String gid) {
+		this.gid = gid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
